9440f39eec8ae5f5e24879248248849870152435
Carlos-Rosas-Orozco <crosasorozco@gmail.com>
2019-10-16 23:05:13 -0500

    first commit

---
+++ IoTAdultosMayores-tesis-v1.0.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IoTAdultosMayores_tesis_v1._0._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
